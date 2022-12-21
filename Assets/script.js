a9604efa96e14ccbc579bb292578c00dda55c339
vbugana <61995940+vbugana@users.noreply.github.com>
2022-12-21 02:02:11 +0000

    Added files and folder
    Add HTML, CSS, JS  updated README

---
+++ Assets/script.js
@@ -0,0 +1,139 @@
+$(window).on('load', function () {
+    currentLocation();
+    checkLocalStorage();
+});
+// API Key for all weather data 
+var APIKey = "300ece5ba5e485c1c1a7f646ea9cc2d4";
+var q = "";
+var now = moment();
+//Date format for header
+var currentDate = now.format('YYYY-MM-YY');
+$("#currentDay").text(currentDate);
+
+//Setting the click function at ID search button
+$("#search-button").on("click", function (event) {
+    // Preventing the button from trying to submit the form
+    event.preventDefault();
+
+    q = $("#city-input").val();
+    if (q === '') {
+        return alert('Please Enter Valid City Name ! ');
+    }
+    getWeather(q);
+
+    saveToLocalStorage(q);
+});
+// Function to create Button for searched city 
+function createRecentSearchBtn(q) {
+    var newLi = $("<li>")
+    var newBtn = $('<button>');
+    //Adding Extra ID for Button to stop Creating Duplicate Button on Click
+    newBtn.attr('id', 'extraBtn');
+    newBtn.addClass("button is-small recentSearch");
+    newBtn.text(q);
+    newLi.append(newBtn)
+    $("#historyList").prepend(newLi);
+    //setting click function to prevent duplicate button
+    $("#extraBtn").on("click", function () {
+        var newQ = $(this).text();
+        getWeather(newQ);
+    });
+}
+//Function to get weather details 
+function getWeather(q) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&units=metric&appid=" + APIKey;
+    $.ajax({
+        // gets the current weather info
+        url: queryURL,
+        method: "GET",
+        error: (err => { //If API through error then alert 
+            alert("Your city was not found. Check your spelling or enter a city code")
+            return;
+          })
+    }).then(function (response) {
+        console.log(response)
+        //to avoid repeating city information on button click 
+        $(".cityList").empty()
+        $("#days").empty()
+        var cityMain1 = $("<div col-12>").append($("<p><h2>" + response.name + ' (' + currentDate + ')' + "</h2><p>"));
+        var image = $('<img class="imgsize">').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');        
+    var degreeMain = $('<p>').text('Temp : ' + response.main.temp + ' °C ');
+        var windMain = $('<p>').text('Wind : ' + response.wind.speed + 'KPH');  
+        var humidityMain = $('<p>').text('Humidity : ' + response.main.humidity + '%');
+        var cityId = response.id;
+        displayForecast(cityId);
+        cityMain1.append(image).append(degreeMain).append(windMain).append(humidityMain);
+        $('#cityList').empty();
+        $('#cityList').append(cityMain1);
+    });
+}
+//function to Display 5 Day forecast
+function displayForecast(c) {
+    $.ajax({ // gets the 5 day forecast API
+        url: "https://api.openweathermap.org/data/2.5/forecast?id=" + c + "&units=metric&APPID=" + APIKey,
+        method: "GET",
+    }).then(function (response) {
+        //  Parse response to display forecast for next 5 days underneath current conditions
+        var arrayList = response.list;
+        for (var i = 0; i < arrayList.length; i++) {
+            if (arrayList[i].dt_txt.split(' ')[1] === '12:00:00') {
+                console.log(arrayList[i]);
+                var cityMain = $('<div>');
+                cityMain.addClass('col forecast bg-primary text-white ml-3 mb-3 rounded>');
+                var date5 = $("<h5>").text(response.list[i].dt_txt.split(" ")[0]);
+                var image = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + arrayList[i].weather[0].icon + '.png');
+                var degreeMain = $('<p>').text('Temp : ' + arrayList[i].main.temp + ' °C');  
+                var windMain = $('<p>').text('Wind : ' + arrayList[i].wind.speed + ' KPH');
+                var humidityMain = $('<p>').text('Humidity : ' + arrayList[i].main.humidity + '%');                
+            cityMain.append(date5).append(image).append(degreeMain).append(windMain).append(humidityMain);
+                $('#days').append(cityMain);
+            }
+        }
+    });
+};
+// Display automatic Current Locaion 
+function currentLocation() {
+    $.ajax({
+        url: "https://freegeoip.app/json/",
+        method: "GET",
+    }).then(function (response) {
+        q = response.city || 'exton';
+        console.log(q);
+        getWeather(q);
+    });
+};
+
+// Function to get data store in local Storage 
+function checkLocalStorage() {
+    var storedData = localStorage.getItem('queries');
+    var dataArray = [];
+    if (!storedData) {
+        console.log("no data stored");
+    } else {
+        storedData.trim();
+        dataArray = storedData.split(',');
+        for (var i = 0; i < dataArray.length; i++) {
+            createRecentSearchBtn(dataArray[i]);
+        }
+    }
+};
+// Function to Set data in Local storage
+function saveToLocalStorage(q) {
+    var data = localStorage.getItem('queries');
+    if (data) {
+        console.log(data, q)
+
+    } else {
+        data = q;
+        localStorage.setItem('queries', data);
+    }
+    if (data.indexOf(q) === -1) {
+        data = data + ',' + q;
+        localStorage.setItem('queries', data);
+        createRecentSearchBtn(q);
+    }
+}
+//added clear histor fuction to clear searched city list
+$("#clear-history").on("click", function (event) {
+    $("#historyList").empty();
+});
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Weather-Dashboard
- Server-Side APIs application
+# Server-Side APIs: Weather Dashboard
+
+## Purpose
+This project is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+## User Story
+
+```text
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Acceptance Criteria
+
+* Create a weather dashboard with form inputs.
+  * When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
+  * When a user views the current weather conditions for that city they are presented with:
+    * The city name
+    * The date
+    * An icon representation of weather conditions
+    * The temperature
+    * The humidity
+    * The wind speed
+  * When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
+    * The date
+    * An icon representation of weather conditions
+    * The temperature
+    * The humidity
+  * When a user click on a city in the search history they are again presented with current and future conditions for that city
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for London.](./Assets/10-server-side-apis-challenge-demo.png)
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/javascript)
+* [Moment.js](https://momentjs.com/docs/)
+* [Bootstrap](https://getbootstrap.com/)
+
+## License
+This project is licensed under the MIT License
